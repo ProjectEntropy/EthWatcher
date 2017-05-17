f8480d9008c9cbb0c9d002cba1eace79b1b67034
Joran <joran.k@gmail.com>
2017-05-17 17:40:47 +1200

    debugging

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ program
 .arguments('<contract_address>')
 .option('-e, --event <event>', 'The event name to watch for')
 .option('-a, --abi <abi>', 'Path to the ABI definition')
-.action(function(address) {
-  console.log(console.log(chalk.bold.cyan(`Watching address: ${address}`)))
+.action(function(address, event, abi) {
+  console.log(chalk.bold.cyan(`Watching address: ${address}`))
+  console.log(chalk.bold.cyan(`       for event: ${event}`))
 })
 .parse(process.argv);
